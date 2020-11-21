14e9a5e26fdd0873f78e69a76408dfbe91d7d1ce
Mo Salehi <mo@mohameds-air.localdomain>
2020-11-21 03:22:14 +0100

    Add a new line has been implemented

--- src/public/static/app.jsx
+++ src/public/static/app.jsx
@@ -1,63 +1,60 @@
-'use strict';
-
-
-
-
+"use strict";
 
 class LineItem extends React.Component {
   render() {
     return (
       <React.Fragment>
-        {this.props.invoiceItem.map((invoiceItem) => (
-          <tr key={invoiceItem.id}>
-            <td>{invoiceItem.description}</td>
-            <td>{invoiceItem.price}</td>
+        {this.props.lineItems.map((lineItems, i) => (
+          <tr key={i}>
+            <td>{lineItems.description}</td>
+            <td>{lineItems.price}</td>
           </tr>
         ))}
       </React.Fragment>
     );
   }
 }
-
 class Invoice extends React.Component {
   state = {
     logoSrc: "/static/images/collectai_logo_green_bg.jpg",
-    invoiceHeadInfo: {
-      invoceNumber: "1566",
-      createdAt: "17/07/2019",
-      dueAt: "17/08/2019",
+    invoice: {
+      email: "",
+      fullName: "",
+      company: "",
+      id: "",
+      createdAt: "",
+      dueAt: "",
+      lineItems: [{ description: "", price: "" }],
     },
-    invoiceInfo: {
-      email: "youknowit@star-wars-is-real.pew",
-      fullName: "Bob Hans Jens, The Great ",
-      company: "Acme, GmbH.",
-    },
-    invoiceItem: [
-      { id: 1, description: "Death StarN", price: "$Crs 20.000.00.0000N" },
-      { id: 2, description: "Death StarN2", price: "$Crs 30.000.00.0000N" },
-      { id: 3, description: "Death StarN3", price: "$Crs 40.000.00.0000N" },
-    ],
-    apiData: [],
-   
+    showAddLine: false,
+    newDesc: "",
+    newPrice: "",
   };
-
-
-  async componentDidMount() {
-    const url = '/api/invoice.json';
-    const response =  await fetch(url);
-    const data = await response.json();
-    this.setState({invoiceInfo:data});
-    this.setState({invoiceItem:data.lineItems});
-    this.setState({invoiceHeadInfo:data});
-    
+  componentDidMount() {
+    fetch("/api/invoice.json")
+      .then((response) => response.json())
+      .then((invoice) => this.setState({ invoice }));
   }
-
+  updateInputValue = (event) => {
+    this.setState({ [event.target.name]: event.target.value });
+  };
+  cancelNewLine = () =>  {
+    this.setState({showAddLine: false });
+  };
+  saveNewLine = () =>  {
+    const invoice = this.state.invoice;
+    const newLine = {description: this.state.newDesc, price: this.state.newPrice}
+    invoice.lineItems.push(newLine);
+    this.setState({ invoice, showAddLine: false, newDesc: "", newPrice: ""});
+  };
+  addNewLine = () => {
+    this.setState({showAddLine: true });
+  };
   render() {
     const logoStyle = {
       width: "100%",
       maxWidth: "300px",
     };
-
     return (
       <table cellPadding="0" cellSpacing="0">
         <tbody>
@@ -71,19 +68,17 @@ class Invoice extends React.Component {
                     </td>
                     <td>
                       Invoice Nr.:
-                      <span>{this.state.invoiceHeadInfo.invoceNumber}</span>
+                      <span>{this.state.invoice.id}</span>
                       <br />
-                      Created:{" "}
-                      <span>{this.state.invoiceHeadInfo.createdAt}</span>
+                      Created: <span>{this.state.invoice.createdAt}</span>
                       <br />
-                      Due: <span>{this.state.invoiceHeadInfo.dueAt}</span>
+                      Due: <span>{this.state.invoice.dueAt}</span>
                     </td>
                   </tr>
                 </tbody>
               </table>
             </td>
           </tr>
-
           <tr className="information">
             <td colSpan="2">
               <table>
@@ -97,35 +92,82 @@ class Invoice extends React.Component {
                       Hamburg, Germany
                     </td>
                     <td>
-                      {this.state.invoiceInfo.company}
+                      {this.state.invoice.company}
                       <br />
-                      {this.state.invoiceInfo.fullName} <br />
-                      {this.state.invoiceInfo.email}
+                      {this.state.invoice.fullName}
+                      <br />
+                      {this.state.invoice.email}
                     </td>
                   </tr>
                 </tbody>
               </table>
             </td>
           </tr>
-
           <tr className="heading">
             <td>Item</td>
 
             <td>Price</td>
           </tr>
-
-          <LineItem invoiceItem={this.state.invoiceItem} />
-          
+          <LineItem lineItems={this.state.invoice.lineItems} />
+          {this.newLineOpener()}
           <tr className="total">
             <td></td>
-
             <td>Total: $385.00</td>
           </tr>
         </tbody>
       </table>
     );
   }
+  newLineOpener() {
+    let showAddLine = this.state.showAddLine;
+    if (showAddLine)
+      return (
+        <React.Fragment>
+          <tr className="newLineForm">
+            <td>
+              <input
+                name="newDesc"
+                type="text"
+                className="full-width"
+                value={this.state.newDesc}
+                onChange={this.updateInputValue}
+              />
+            </td>
+            <td>
+              <input
+                name="newPrice"
+                type="text"
+                className="full-width"
+                value={this.state.newPrice}
+                onChange={this.updateInputValue}
+              />
+            </td>
+          </tr>
+          <tr className="newLineForm">
+            <td>
+              <button onClick={this.saveNewLine} className="button-primary">
+                Save
+              </button>
+              <button onClick={this.cancelNewLine} className="button-primary">
+                Cancel
+              </button>
+            </td>
+            <td></td>
+          </tr>
+        </React.Fragment>
+      );
+    else
+      return (
+        <tr className="newLine">
+          <td>
+            <button onClick={this.addNewLine} className="button-primary">
+              New Line
+            </button>
+          </td>
+          <td></td>
+        </tr>
+      );
+  };
 }
-
-const domContainer = document.querySelector('#invoice');
-ReactDOM.render(<Invoice/>, domContainer);
+const domContainer = document.querySelector("#invoice");
+ReactDOM.render(<Invoice />, domContainer);
